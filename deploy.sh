7976f2868acaf55b0f57d2a17944915c04fef821
27 <zhengchengzhong@yuewen.com>
2019-11-19 21:35:43 +0800

    docs: update docs dev

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ set -e
 yarn build
 
 git add -A
-git commit -m 'docs: update docs'
+git commit -m 'docs: update docs dev'
 git push -f
 
 cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'docs: update docs'
+git commit -m 'docs: update docs master'
 
 git push -f git@github.com:nu-system/nu-system.github.io master
 
